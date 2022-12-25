a4211e5903f346445c5c0cb885b3de2f9798484a
synthetic-borealis <snip.coloured@gmail.com>
2022-12-25 12:50:21 +0200

    fix: correct response string

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ let express = require('express');
 let app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hello World');
+    res.send('Hello Express');
 });
 
 module.exports = app;
