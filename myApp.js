d3bb126196f001678f02d35ffffb553c5514e690
synthetic-borealis <snip.coloured@gmail.com>
2022-12-25 13:03:39 +0200

    step 4: add json route

--- myApp.js
+++ myApp.js
@@ -6,6 +6,12 @@ const indexPath = __dirname + '/views/index.html';
 
 app.use('/public', express.static(staticPath));
 
+app.get('/json', function (req, res) {
+    res.json({
+        message: 'Hello json',
+    });
+});
+
 app.get('/', function (req, res) {
     res.sendFile(indexPath);
 });
