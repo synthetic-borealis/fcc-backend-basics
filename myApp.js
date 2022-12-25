69df3f7ffeabfcc8aa2efef64dc82eedd90e2485
synthetic-borealis <snip.coloured@gmail.com>
2022-12-25 13:12:09 +0200

    step 5: add dotenv

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let app = express();
+require('dotenv').config();
 
 const staticPath = __dirname + '/public';
 const indexPath = __dirname + '/views/index.html';
@@ -7,9 +8,15 @@ const indexPath = __dirname + '/views/index.html';
 app.use('/public', express.static(staticPath));
 
 app.get('/json', function (req, res) {
-    res.json({
-        message: 'Hello json',
-    });
+    if (process.env.MESSAGE_STYLE === 'uppercase') {
+        res.json({
+            message: 'Hello json'.toUpperCase(),
+        });
+    } else {
+        res.json({
+            message: 'Hello json',
+        });
+    }
 });
 
 app.get('/', function (req, res) {
